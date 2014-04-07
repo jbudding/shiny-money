ec7d96950881702b75c80261cf1f2702cf722de2
abuddington <austin.j.buddington@gmail.com>
2014-04-07 14:51:05 -0400

    added a hash to track our data

--- ruby-scripts/bank.rb
+++ ruby-scripts/bank.rb
@@ -1,25 +1,49 @@
 class BankAccount
-	
-	def initialize(name)
-		@transactions = []
-		@balance = 0
-	end 
 
-	def deposit
+	def initialize(first_name, last_name)
+
+		# make a object (in ruby it's called a Hash) of all of the info we need,
+		# this way it's all in a nice group instead of tons of individual variables
+		@attr = Hash.new()
+
+		@attr["first_name"] = first_name
+
+		@attr["last_name"] = last_name
+
+		@attr["transactions"] = []
+
+		@attr["balance"] = 0
+
+	end
+
+	def deposit(amount)
+
 		puts "How much would you like to deposit?"
+
 		amount = gets.chomp
-		@balance += amount.to_f
-		puts "$#{amount} has been deposited."
+
+		# access attribures as an object
+		# i think you can also do @attr.balance
+		@attr["balance"] += amount.to_f
+
+		message = "$#{amount} has been deposited."
+
+		# push this transaction to the end of the transactions array
+		@attr["transactions"] << message
+
+		puts message
+
 	end
-	
+
 	def show_balance
-		puts "Your current balance is #{@balance}"
+
+		puts "Your current balance is $#{@attr["balance"]}"
+
 	end
+
 end
 
-bank_account = BankAccount.new("Chocolate Tomato")
-bank_account.class
+bank_account = BankAccount.new("Chocolate", "Tomato")
 
-bank_account.deposit
+bank_account.deposit(200)
 bank_account.show_balance
-	
