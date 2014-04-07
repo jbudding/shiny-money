8926d534996e50799ad5600aa5e05fb7239dcabc
Austin Buddington <austin.j.buddington@gmail.com>
2014-04-07 04:17:29 -0400

    fixed comments

--- blake.rb
+++ blake.rb
@@ -1,3 +1,6 @@
-Hello Austin
-below is some ruby code below pertaining to our project
+# Hello Austin
+# below is some ruby code below pertaining to our project
 puts 5 + 5  
+
+# Don't forget to use the '#' symbol at the beginning of a line
+# to use comments
