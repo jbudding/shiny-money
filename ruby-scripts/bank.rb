ef47e74345dd92c151fc4af51b3a033d207adb5e
abuddington <austin.j.buddington@gmail.com>
2014-04-19 17:15:01 -0400

    updates

--- ruby-scripts/bank.rb
+++ ruby-scripts/bank.rb
@@ -7,9 +7,7 @@ class BankAccount
 
     def initialize(first_name, last_name)
 
-        @first_name = first_name
-
-        @last_name = last_name
+        @first_name, @last_name  = first_name, last_name
 
         @transactions = []
 
